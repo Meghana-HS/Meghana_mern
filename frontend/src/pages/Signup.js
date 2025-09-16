4bb15e4f2b6e795bd500f5c178ba0f9f9a48bad0
meghana HS <hmeghana719@gmail.com>
2025-09-16 19:45:25 +0530

    Update Signup.js

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -27,7 +27,7 @@ function Signup() {
             return handleError('name, email and password are required')
         }
         try {
-            const url = "http://localhost:3000/auth/signup";
+            const url = "https://meghana-mern.vercel.app/auth/signup";
             
             const response = await fetch(url, {
                 method: "POST",
