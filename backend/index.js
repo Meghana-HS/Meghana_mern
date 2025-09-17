07a9b37e7c965520d542f1c6b2409a12b06f2bc9
meghana HS <hmeghana719@gmail.com>
2025-09-17 17:20:50 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 
 
 const allowedOrigins = [
-    "https://meghana-mern-frontend.onrender.com",
+   "https://meghana-mern-1.onrender.com" ,
   "https://meghana-mern-bctw-5nadawiez-meghana-hss-projects.vercel.app",
   "http://localhost:3000"
 ];
